547697739c355f72590cfab5e661aa6942a3ba36
mateusTegue <teusteguetorres2001@gmail.com>
2026-03-26 18:58:46 -0500

    Remove dotenv import and loading from settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 from pathlib import Path
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
